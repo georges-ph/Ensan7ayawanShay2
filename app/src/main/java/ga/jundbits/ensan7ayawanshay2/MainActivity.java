c2761ea82695d1e5f0b718399dda78225ca0eb21
georges-ph <99219167+georges-ph@users.noreply.github.com>
2022-11-22 18:09:29 +0200

    created new empty project with another package name

---
+++ app/src/main/java/ga/jundbits/ensan7ayawanshay2/MainActivity.java
@@ -0,0 +1,14 @@
+package ga.jundbits.ensan7ayawanshay2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
